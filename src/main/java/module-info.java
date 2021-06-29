ecc75f9ab48df1f4c923c350b895ba4e95efdf4f
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:57:05 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,28 @@
+/*
+ * ao-web-resources-servlet - Web resource management in a Servlet environment.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-web-resources-servlet.
+ *
+ * ao-web-resources-servlet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-web-resources-servlet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-web-resources-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoapps.web.resources.servlet {
+	exports com.aoapps.web.resources.servlet;
+	// Direct
+	requires com.aoapps.web.resources.registry; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-registry</artifactId>
+	requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
+}
