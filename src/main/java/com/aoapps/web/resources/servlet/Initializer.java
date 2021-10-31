c4dde646d1f1df2aed9ece5161bef434c821bc22
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:55:38 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/com/aoapps/web/resources/servlet/Initializer.java
+++ src/main/java/com/aoapps/web/resources/servlet/Initializer.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-web-resources-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-web-resources-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.aoapps.web.resources.servlet;
 
