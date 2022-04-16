d0c65b6b795cde4140fe4378bea222ab6120b23a
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:19 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/web/resources/servlet/RegistryEE.java
+++ src/main/java/com/aoapps/web/resources/servlet/RegistryEE.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-web-resources-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.web.resources.servlet;
 
 import com.aoapps.servlet.attribute.ScopeEE;
